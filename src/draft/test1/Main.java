950d54a7c1485ba35078d5509153a5830d5bd8d0
ttnamnktp <tthanhnam.nktpglobal@gmail.com>
2024-01-14 11:46:08 +0700

    first commit

---
+++ src/draft/test1/Main.java
@@ -0,0 +1,14 @@
+package test1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        A a = new A();
+
+        int ig = 0;
+        String b = "o";
+
+        a.firstMethod(b, ig);
+    }
+}
